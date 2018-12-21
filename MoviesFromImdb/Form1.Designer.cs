a4dd13c6c9b14367f31e7b2f1ae45a47f9ed8fb2
luka.jovic <luka.jovic93@gmail.com>
2018-12-21 16:19:23 +0100

    Add project files.

---
+++ MoviesFromImdb/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace MoviesFromImdb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.tbYear = new System.Windows.Forms.TextBox();
+            this.tbRated = new System.Windows.Forms.TextBox();
+            this.tbReleased = new System.Windows.Forms.TextBox();
+            this.tbGenre = new System.Windows.Forms.TextBox();
+            this.tbActors = new System.Windows.Forms.TextBox();
+            this.pbPoster = new System.Windows.Forms.PictureBox();
+            this.tbPlot = new System.Windows.Forms.TextBox();
+            this.tbMetascore = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPoster)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(211, 35);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(337, 20);
+            this.tbSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(349, 70);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Location = new System.Drawing.Point(97, 115);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.ReadOnly = true;
+            this.tbTitle.Size = new System.Drawing.Size(451, 20);
+            this.tbTitle.TabIndex = 3;
+            // 
+            // tbYear
+            // 
+            this.tbYear.Location = new System.Drawing.Point(97, 151);
+            this.tbYear.Name = "tbYear";
+            this.tbYear.ReadOnly = true;
+            this.tbYear.Size = new System.Drawing.Size(451, 20);
+            this.tbYear.TabIndex = 4;
+            // 
+            // tbRated
+            // 
+            this.tbRated.Location = new System.Drawing.Point(97, 229);
+            this.tbRated.Name = "tbRated";
+            this.tbRated.ReadOnly = true;
+            this.tbRated.Size = new System.Drawing.Size(451, 20);
+            this.tbRated.TabIndex = 5;
+            // 
+            // tbReleased
+            // 
+            this.tbReleased.Location = new System.Drawing.Point(97, 192);
+            this.tbReleased.Name = "tbReleased";
+            this.tbReleased.ReadOnly = true;
+            this.tbReleased.Size = new System.Drawing.Size(451, 20);
+            this.tbReleased.TabIndex = 6;
+            // 
+            // tbGenre
+            // 
+            this.tbGenre.Location = new System.Drawing.Point(97, 334);
+            this.tbGenre.Name = "tbGenre";
+            this.tbGenre.ReadOnly = true;
+            this.tbGenre.Size = new System.Drawing.Size(451, 20);
+            this.tbGenre.TabIndex = 7;
+            // 
+            // tbActors
+            // 
+            this.tbActors.Location = new System.Drawing.Point(97, 300);
+            this.tbActors.Name = "tbActors";
+            this.tbActors.ReadOnly = true;
+            this.tbActors.Size = new System.Drawing.Size(451, 20);
+            this.tbActors.TabIndex = 8;
+            // 
+            // pbPoster
+            // 
+            this.pbPoster.Location = new System.Drawing.Point(573, 115);
+            this.pbPoster.Name = "pbPoster";
+            this.pbPoster.Size = new System.Drawing.Size(202, 328);
+            this.pbPoster.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPoster.TabIndex = 9;
+            this.pbPoster.TabStop = false;
+            // 
+            // tbPlot
+            // 
+            this.tbPlot.Location = new System.Drawing.Point(97, 368);
+            this.tbPlot.Multiline = true;
+            this.tbPlot.Name = "tbPlot";
+            this.tbPlot.ReadOnly = true;
+            this.tbPlot.Size = new System.Drawing.Size(451, 75);
+            this.tbPlot.TabIndex = 10;
+            // 
+            // tbMetascore
+            // 
+            this.tbMetascore.Location = new System.Drawing.Point(97, 263);
+            this.tbMetascore.Name = "tbMetascore";
+            this.tbMetascore.ReadOnly = true;
+            this.tbMetascore.Size = new System.Drawing.Size(451, 20);
+            this.tbMetascore.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Year";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Rated";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Released";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 334);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Genre";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 300);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Actors";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 263);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Metascore";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(27, 368);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Plot";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(333, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(116, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Enter the movie name :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbMetascore);
+            this.Controls.Add(this.tbPlot);
+            this.Controls.Add(this.pbPoster);
+            this.Controls.Add(this.tbActors);
+            this.Controls.Add(this.tbGenre);
+            this.Controls.Add(this.tbReleased);
+            this.Controls.Add(this.tbRated);
+            this.Controls.Add(this.tbYear);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.tbSearch);
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Search for Movie";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPoster)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.TextBox tbYear;
+        private System.Windows.Forms.TextBox tbRated;
+        private System.Windows.Forms.TextBox tbReleased;
+        private System.Windows.Forms.TextBox tbGenre;
+        private System.Windows.Forms.TextBox tbActors;
+        private System.Windows.Forms.PictureBox pbPoster;
+        private System.Windows.Forms.TextBox tbPlot;
+        private System.Windows.Forms.TextBox tbMetascore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ MoviesFromImdb/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web.Script.Serialization;
+
+namespace MoviesFromImdb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string url = "http://www.omdbapi.com/?t=" + tbSearch.Text.Trim() + "&apikey=e17f08db";
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString(url);
+                JavaScriptSerializer oJS = new JavaScriptSerializer();
+                ImdbEntity obj = new ImdbEntity();
+                obj = oJS.Deserialize<ImdbEntity>(json);
+                if (obj.Response == "True")
+                {
+                    tbTitle.Text = obj.Title;
+                    tbYear.Text = obj.Year;
+                    tbRated.Text = obj.imdbRating;
+                    tbReleased.Text = obj.Released;
+                    tbGenre.Text = obj.Genre;
+                    tbActors.Text = obj.Actors;
+                    tbPlot.Text = obj.Plot;
+                    tbMetascore.Text = obj.Metascore;
+                    pbPoster.ImageLocation = obj.Poster;
+
+
+    }
+                else
+                {
+                    MessageBox.Show("Movie not Found!!!");
+                }
+
+
+            }
+        }
+    }
+}
